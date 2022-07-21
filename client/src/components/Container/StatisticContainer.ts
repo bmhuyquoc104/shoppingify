47966aff2438f088efdcefa823e6fe4e70344f1c
Huy ne <bmhuyquoc104@gmail.com>
2022-07-21 18:19:30 +0700

    71. Implement line graph for monthly sale

---
+++ client/src/components/Container/StatisticContainer.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+const StatisticContainerStyled = styled.div`
+  display: grid;
+  margin-left: 6em;
+  gap: 1em;
+  width: 100%;
+  grid-template-columns: minmax(350px, 1fr) 24em;
+  /* grid-template-rows: 16vh 1fr; */
+  grid-template-areas:
+    "search-section shopping-cart"
+    "item-list  shopping-cart";
+  & :first-child {
+    grid-area: search-section;
+  }
+
+  & :nth-child(2) {
+    grid-area: item-list;
+  }
+
+  &:nth-child(3) {
+    grid-area: shopping-cart;
+  }
+  @media (max-width: 1137px) {
+    grid-template-columns: minmax(500px, 1fr) 24em;
+  }
+  
+  @media (max-width: 909px) {
+    grid-template-columns: minmax(350px, 1fr) 24em;
+  }
+  @media (max-width:750px){
+    margin-left: 5.2em;
+    grid-template-columns:1fr;
+    grid-template-areas:
+    "search-section"
+    "item-list"
+    "shopping-cart";
+  }
+  @media (max-width:480px){
+    margin-left:3.5em;
+  }
+`;
+
+export default StatisticContainerStyled;
